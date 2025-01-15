0086e4a92b64fb261db4e19f2e00615e118da928
Nosuke314 <okadanosuke1213@gmail.com>
2025-01-16 08:40:14 +0900

    Update Kaito.js

--- Kaito.js
+++ Kaito.js
@@ -1,1 +1,7 @@
+// script.js
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
 
+hamburger.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
