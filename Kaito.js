bd68b4bb6a757e3cd7b7961bc7b246d2fed9f901
Nosuke314 <okadanosuke1213@gmail.com>
2025-01-16 09:47:04 +0900

    Update Kaito.js

--- Kaito.js
+++ Kaito.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
 
-// メニューアイテムを選択
-const menuItems = document.querySelectorAll(".menu a");
+  // メニューアイテムを選択
+  const menuItems = document.querySelectorAll(".menu a");
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("show");
-});
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
 
-// メニューアイテムをクリックしたときにスクロールアニメーション
-menuItems.forEach(item => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault(); // リンクのデフォルトの動作を無効にする
-    const targetId = e.target.getAttribute("href").slice(1); // #を取り除く
-    const targetElement = document.getElementById(targetId);
+  // メニューアイテムをクリックしたときにスクロールアニメーション
+  menuItems.forEach(item => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault(); // リンクのデフォルトの動作を無効にする
+      const targetId = e.target.getAttribute("href").slice(1); // #を取り除く
+      const targetElement = document.getElementById(targetId);
 
-    targetElement.scrollIntoView({
-      behavior: 'smooth', // スムーズなスクロール
-      block: 'start' // 上部に合わせてスクロール
+      targetElement.scrollIntoView({
+        behavior: 'smooth', // スムーズなスクロール
+        block: 'start' // 上部に合わせてスクロール
+      });
+
+      menu.classList.remove("show"); // メニューを閉じる
     });
-
-    menu.classList.remove("show"); // メニューを閉じる
   });
 });
