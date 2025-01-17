6a97aa59ff0a1bc4edb57614064908d0ee23b580
Nosuke314 <okadanosuke1213@gmail.com>
2025-01-18 08:51:58 +0900

    Update a7.js

--- kaito/a7.js
+++ kaito/a7.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const tabButton = document.querySelector('.tab-button');
-  const tabContent = document.querySelector('.tab-content');
 
-  tabButton.addEventListener('click', () => {
-    if (tabContent.style.maxHeight) {
-      // タブを閉じる
-      tabContent.style.maxHeight = null;
-    } else {
-      // タブを開く
-      tabContent.style.maxHeight = tabContent.scrollHeight + 'px';
-    }
-  });
-});
